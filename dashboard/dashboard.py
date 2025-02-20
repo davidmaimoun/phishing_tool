f07c487affc4483f0d4044b7f96b5d0e82b182c5
davidmaimoun <31177421+davidmaimoun@users.noreply.github.com>
2025-02-20 16:09:26 +0200

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,6 +1,8 @@
-import sqlite3
 import os
+import sqlite3
 import pandas as pd
+import streamlit as st
+
 
 DB_DIR = "/home/user1/Desktop/programs/phisingTool/backend/dbs" 
 
@@ -18,21 +20,32 @@ def fetch_data_from_db(db_name):
     conn.close()
     return df
 
-def fetch_all_data():
-    all_data = {}
-    for db_file in os.listdir(DB_DIR):
+def fetch_db_names(db_dir):
+    db_names = []
+    for db_file in os.listdir(db_dir):
         if db_file.endswith(".db"):
-            db_name = db_file
-            data = fetch_data_from_db(db_name)
+            db_names.append(db_file)
+                  
+    return db_names
+
+def fetch_all_data(dbs):
+    all_data = {}
+    for db_name in dbs:
+            db_file_full = f"{db_name}.db"
+            data = fetch_data_from_db(db_file_full)
             if data is not None:
                 all_data[db_name] = data
     return all_data
 
-# Example: Get data for each file
-all_data = fetch_all_data()
 
-# If using Streamlit for a dashboard
-import streamlit as st
-for db_name, df in all_data.items():
-    st.write(f"Data from {db_name}:")
-    st.dataframe(df)
+dbs_chosen = []
+with st.sidebar:
+    dbs = [os.path.splitext(f)[0] for f in os.listdir(DB_DIR) if f.endswith(".db")]
+
+    dbs_chosen = st.multiselect('Place', dbs)
+
+if dbs is not None:
+    all_data = fetch_all_data(dbs_chosen)
+    for db_name, df in all_data.items():
+        st.write(f"Data from {db_name}:")
+        st.dataframe(df)
